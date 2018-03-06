e7471fc6c1250d341afb77228303ecb11f2d8367
Peter Große <pegro@friiks.de>
2018-03-06 18:08:39 +0100

    ath10k: debug: hide UPDATE_STATS_EVENTID events, add message after interface configuration

--- drivers/net/wireless/ath/ath10k/wmi.c
+++ drivers/net/wireless/ath/ath10k/wmi.c
@@ -3105,7 +3105,7 @@ static int ath10k_wmi_10_4_op_pull_fw_stats(struct ath10k *ar,
 
 void ath10k_wmi_event_update_stats(struct ath10k *ar, struct sk_buff *skb)
 {
-	ath10k_dbg(ar, ATH10K_DBG_WMI, "WMI_UPDATE_STATS_EVENTID\n");
+	//ath10k_dbg(ar, ATH10K_DBG_WMI, "WMI_UPDATE_STATS_EVENTID\n");
 	ath10k_debug_fw_stats_process(ar, skb);
 }
 
@@ -3139,6 +3139,9 @@ void ath10k_wmi_event_vdev_start_resp(struct ath10k *ar, struct sk_buff *skb)
 		ath10k_warn(ar, "failed to parse vdev start event: %d\n", ret);
 		return;
 	}
+
+	ath10k_dbg(ar, ATH10K_DBG_WMI, "resp: vdev_id %d req_id: %d resp_type: %d status: %d\n",
+		__le32_to_cpu(arg.vdev_id), __le32_to_cpu(arg.req_id), __le32_to_cpu(arg.resp_type), __le32_to_cpu(arg.status));
 
 	if (WARN_ON(__le32_to_cpu(arg.status)))
 		return;
